39ca63eaae175feb99d1f008c938389bef58e875
Andres Reyes <andresreyes@MacBook-Pro-de-Andres.local>
2023-11-01 09:40:31 -0300

    Commit para dployment en netlify

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+
+
+    return(
+        <div className="">
+            <h1 className="font-black text-4xl text-center  md:w-2/2 mx-auto">Seguimiento Pacientes <span className="text-indigo-600"> Veterinaria</span></h1>
+            
+        </div>
+    )
+}
+
+export default Header;
